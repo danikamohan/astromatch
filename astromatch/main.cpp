1eec330327dd1165f2f0db76c8f867a9f54a3ab6
luvrani <danikamohan@gmail.com>
2026-03-03 16:01:34 -0800

    final version

--- astromatch/main.cpp
+++ astromatch/main.cpp
@@ -1,18 +1,23 @@
 //
-//  main.cpp
-//  astromatch
+//  astromatch.cpp
+//  AstroMatch
 //
 //  Created by Danika on 1/30/26.
+//  Welcome to AstroMatch, where you find out if you are astrologically compatible with your partner!
 //
 
 #include <iostream>
 #include <string>
 using namespace std;
 
-string element(string sign);
+string element(string sign); // function used to find out what element your sign is!
+string findMatch(string element1, string element2); // used to determine your true match!
+// main function that gets two signs from user, shows them their sign element, and shows them
+// compatibilty result!
 int main() {
     string zodiac_one, zodiac_two;
     string findElement1, findElement2;
+    string result;
     
     cout << "------------------------------------------\n"
     << " Welcome to the zodiac compatibility test!\n"
@@ -27,7 +32,15 @@ int main() {
     findElement1 = element(zodiac_one);
     findElement2 = element(zodiac_two);
     
+    if (findElement1 == "Unknown" || findElement2 == "Unknown") {
+        cout << " Invalid sign entered. Please try again." << endl;
+        return 0;
+    }
     cout << " Your elements are " << findElement1 << " and " << findElement2 << "." << endl;
+    
+    result = findMatch(findElement1, findElement2);
+    
+    cout << " Compatibility result = " << result << endl;
     
     return 0;
 }
@@ -52,11 +65,18 @@ string element(string sign) {
 
 string findMatch(string element1, string element2) {
     if (element1 == element2) {
-        return "Wow! Great Match! Same energy!";
+        return "Wow! Strong match! Same energy!";
     }
     else if ((element1 == "Fire" && element2 == "Air") || (element1 == "Air" && element2 == "Fire")) {
         return "Good match! You bring out the best in eachother!";
     }
     else if ((element1 == "Water" && element2 == "Earth") || (element1 == "Earth" && element2 == "Water")) {
         return "Good match! You are grounding and nurturing with eachother!";
+    }
+    else if ((element1 == "Fire" && element2 == "Water") || (element1 == "Water" && element2 == "Fire")) {
+        return "Hmm tricky match! you both are opposites that can clash.";
+    }
+    else {
+        return "Neutral match, your relationship might take some effort!";
+    }
 }
